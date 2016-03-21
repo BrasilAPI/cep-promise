e452838077cb2321f33872ab14c6d707415374c2
Filipe Deschamps <filiped@gmail.com>
2016-03-21 08:16:18 -0300

    feat(validation): melhora validação de entrada

--- src/cep-promise.js
+++ src/cep-promise.js
@@ -3,17 +3,26 @@
 export default function (cepRawValue) {
   return new Promise((resolve, reject) => {
     validateInput(cepRawValue)
+      .then(finish)
       .catch(reject)
 
     function validateInput (cepRawValue) {
       return new Promise(function (resolve, reject) {
-        if (!cepRawValue) {
-          reject({
-            type: 'type_error',
-            message: 'You need to call the constructor with a String.'
-          })
+        var cepTypeOf = typeof cepRawValue
+
+        if (cepTypeOf === 'number' || cepTypeOf === 'string') {
+          return resolve(cepRawValue)
         }
+
+        return reject({
+          type: 'type_error',
+          message: 'You need to call the constructor with a String or Number.'
+        })
       })
+    }
+
+    function finish (cepResult) {
+      resolve(cepResult)
     }
   })
 }
