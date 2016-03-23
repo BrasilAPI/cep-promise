68e05199c978f807ff5b576955349e14e7475224
Filipe Deschamps <filiped@gmail.com>
2016-03-23 06:56:38 -0300

    feat(normalization): adiciona função que remove caracteres especiais

--- src/cep-promise.js
+++ src/cep-promise.js
@@ -3,6 +3,7 @@
 export default function (cepRawValue) {
   return new Promise((resolve, reject) => {
     validateInput(cepRawValue)
+      .then(removeSpecialCharacters)
       .then(finish)
       .catch(reject)
 
@@ -18,6 +19,13 @@ export default function (cepRawValue) {
           type: 'type_error',
           message: 'You need to call the constructor with a String or Number.'
         })
+      })
+    }
+
+    function removeSpecialCharacters (cepRawValue) {
+      return new Promise((resolve, reject) => {
+        let cepCleanValue = cepRawValue.toString().replace(/[^0-9]+/ig, '')
+        resolve(cepCleanValue)
       })
     }
 
