93ce58b9d36d944966c9d9830fb4e6c9a5a6c12f
Filipe Deschamps <filiped@gmail.com>
2016-06-05 10:23:04 -0300

    feat(fetch): fetch, parse and resolve with address values

--- src/cep-promise.js
+++ src/cep-promise.js
@@ -1,11 +1,17 @@
 'use strict'
 
+var http = require('https')
+var parseXMLString = require('xml2js').parseString
+
 export default function (cepRawValue) {
   return new Promise((resolve, reject) => {
     Promise.resolve(cepRawValue)
       .then(validateInput)
       .then(removeSpecialCharacters)
       .then(leftPadWithZeros)
+      .then(fetchCorreiosService)
+      .then(parseXML)
+      .then(extractValuesFromParsedXML)
       .then(finish)
       .catch(handleError)
 
@@ -35,8 +41,59 @@ export default function (cepRawValue) {
       return cepWithLeftPad
     }
 
-    function finish (cepResult) {
-      resolve(cepResult)
+    function fetchCorreiosService (cepWithLeftPad) {
+      return new Promise(function (resolve, reject) {
+        var options = {
+          'method': 'POST',
+          'hostname': 'apps.correios.com.br',
+          'path': '/SigepMasterJPA/AtendeClienteService/AtendeCliente',
+          'headers': {
+            'content-type': 'text/xml;charset=utf-8',
+            'cache-control': 'no-cache'
+          }
+        }
+
+        var req = http.request(options, function (res) {
+          var chunks = []
+
+          res.on('data', function (chunk) {
+            chunks.push(chunk)
+          })
+
+          res.on('end', function () {
+            var body = Buffer.concat(chunks)
+            return resolve(body.toString())
+          })
+        })
+
+        req.write('<?xml version="1.0"?>\n<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cli="http://cliente.bean.master.sigep.bsb.correios.com.br/">\n  <soapenv:Header />\n  <soapenv:Body>\n    <cli:consultaCEP>\n      <cep>' + cepWithLeftPad + '</cep>\n    </cli:consultaCEP>\n  </soapenv:Body>\n</soapenv:Envelope>')
+        req.end()
+      })
+    }
+
+    function parseXML (xmlString) {
+      return new Promise(function (resolve, reject) {
+        parseXMLString(xmlString, function (err, xmlObject) {
+          resolve(xmlObject)
+        })
+      })
+    }
+
+    function extractValuesFromParsedXML (xmlObject) {
+      var addressValues = xmlObject['soap:Envelope']['soap:Body'][0]['ns2:consultaCEPResponse'][0]['return'][0]
+      var addressObject = {
+        cep: addressValues['cep'][0],
+        state: addressValues['uf'][0],
+        city: addressValues['cidade'][0],
+        neighborhood: addressValues['bairro'][0],
+        street: addressValues['end'][0]
+      }
+
+      return addressObject
+    }
+
+    function finish (addressObject) {
+      resolve(addressObject)
     }
 
     function handleError (error) {
