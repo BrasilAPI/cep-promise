e274c78a714f5b8f1c817959e01c87eb5f6b5a06
Luciano Pellacani Franca <lucianopf@outlook.com>
2016-09-14 18:28:10 +0000

    Adding tests for the fallback feature

--- test/unit/cep-promise.spec.js
+++ test/unit/cep-promise.spec.js
@@ -93,7 +93,7 @@ describe('cep-promise (unit)', () => {
   })
 
   describe('when invoked with an inexistend "99999999" cep', () => {
-    it('should reject with "type_error"', () => {
+    it('should reject with "range_error"', () => {
       nock('https://apps.correios.com.br')
         .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
         .replyWithFile(500, path.join(__dirname, '/fixtures/response-cep-not-found.xml'))
@@ -106,7 +106,7 @@ describe('cep-promise (unit)', () => {
   })
 
   describe('when invoked with an inexistend "1" cep', () => {
-    it('should reject with "type_error"', () => {
+    it('should reject with "range_error"', () => {
       nock('https://apps.correios.com.br')
         .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
         .replyWithFile(500, path.join(__dirname, '/fixtures/response-cep-not-found.xml'))
@@ -119,7 +119,7 @@ describe('cep-promise (unit)', () => {
   })
 
   describe('when invoked with an invalid "123456789" cep', () => {
-    it('should reject with "range_error"', () => {
+    it('should reject with "type_error"', () => {
       nock('https://apps.correios.com.br')
         .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
         .replyWithFile(500, path.join(__dirname, '/fixtures/response-cep-invalid-format.xml'))
@@ -144,44 +144,79 @@ describe('cep-promise (unit)', () => {
     })
   })
 
-  describe('when request returns with error but with an unkown XML schema', () => {
+  describe('when request returns with error but with an unkown XML schema and then succeed to the failover service', () => {
     it('should reject with "error"', () => {
       nock('https://apps.correios.com.br')
         .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
         .replyWithFile(500, path.join(__dirname, '/fixtures/response-unknown-format.xml'))
-
-      return expect(cep('05010000')).to.be.rejected.and.to.eventually.deep.equal({
-        type: 'error',
-        message: 'Correios respondeu consulta utilizando um formato de XML desconhecido'
+      nock('https://viacep.com.br')
+        .get('/ws/05010000/json/')
+        .replyWithFile(200, path.join(__dirname, '/fixtures/viacep-cep-05010000-found.json'))
+      return expect(cep('5010000')).to.eventually.deep.equal({
+        cep: '05010000',
+        state: 'SP',
+        city: 'São Paulo',
+        neighborhood: 'Perdizes',
+        street: 'Rua Caiubi'
       })
     })
   })
 
-  describe('when its not possible to parse the returning XML', () => {
-    it('should reject with "error"', () => {
+  describe('when its not possible to parse the returning XML and then succeed to the failover service', () => {
+    it('should fulfill with correct address', () => {
       nock('https://apps.correios.com.br')
         .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
         .replyWithFile(200, path.join(__dirname, '/fixtures/response-bad-xml.xml'))
-
-      return expect(cep('05010000')).to.be.rejected.and.to.eventually.deep.equal({
-        type: 'error',
-        message: 'Correios respondeu consulta utilizando um formato de XML desconhecido'
+      nock('https://viacep.com.br')
+        .get('/ws/05010000/json/')
+        .replyWithFile(200, path.join(__dirname, '/fixtures/viacep-cep-05010000-found.json'))
+      return expect(cep('5010000')).to.eventually.deep.equal({
+        cep: '05010000',
+        state: 'SP',
+        city: 'São Paulo',
+        neighborhood: 'Perdizes',
+        street: 'Rua Caiubi'
       })
     })
   })
 
-  describe('when http request fail', () => {
+  describe('when http request to correios fails and then succeed to the failover service', () => {
+    it('should fulfill with correct address', () => {
+      nock('https://apps.correios.com.br')
+        .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
+        .replyWithError('getaddrinfo ENOTFOUND apps.correios.com.br apps.correios.com.br:443')
+      nock('https://viacep.com.br')
+        .get('/ws/05010000/json/')
+        .replyWithFile(200, path.join(__dirname, '/fixtures/viacep-cep-05010000-found.json'))
+      
+      return expect(cep('5010000')).to.eventually.deep.equal({
+        cep: '05010000',
+        state: 'SP',
+        city: 'São Paulo',
+        neighborhood: 'Perdizes',
+        street: 'Rua Caiubi'
+      })
+    })
+  })
+  
+  describe('when http request fails both for primary and secondary service', () => {
     it('should reject with "error"', () => {
       nock('https://apps.correios.com.br')
         .post('/SigepMasterJPA/AtendeClienteService/AtendeCliente')
         .replyWithError('getaddrinfo ENOTFOUND apps.correios.com.br apps.correios.com.br:443')
-
+      nock('https://viacep.com.br')
+        .get('/ws/05010000/json/')
+        .reply(400, '<h2>Bad Request (400)</h2>')
+        
       return expect(cep('05010000')).to.be.rejected.and.to.eventually.deep.equal({
         type: 'error',
-        message: 'Erro ao se conectar com o serviço dos Correios'
+        message: 'Não foi possível processar o cep requerido'
       })
     })
   })
+
+
+
 
   afterEach(() => {
     nock.cleanAll()
