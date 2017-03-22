5a4f04c7495023f469ac6fdd47a224baac2cbe91
olegon <olegon.main@gmail.com>
2017-03-22 16:33:27 -0300

    Refatoração
    - Removido a criação de uma nova Promise via construtor. O objetivo é deixar o código mais legível. ;)

--- src/cep-promise.js
+++ src/cep-promise.js
@@ -8,86 +8,78 @@ import Promise from './utils/promise-any.js'
 const CEP_SIZE = 8
 
 export default function (cepRawValue) {
-  return new Promise((resolve, reject) => {
-    Promise.resolve(cepRawValue)
-      .then(validateInputType)
-      .then(removeSpecialCharacters)
-      .then(validateInputLength)
-      .then(leftPadWithZeros)
-      .then(fetchCepFromServices)
-      .then(resolvePromise)
-      .catch(handleServicesError)
-      .catch(rejectWithCepPromiseError)
+  return Promise.resolve(cepRawValue)
+    .then(validateInputType)
+    .then(removeSpecialCharacters)
+    .then(validateInputLength)
+    .then(leftPadWithZeros)
+    .then(fetchCepFromServices)
+    .catch(handleServicesError)
+    .catch(throwApplicationError)
+}
 
-    function validateInputType (cepRawValue) {
-      let cepTypeOf = typeof cepRawValue
+function validateInputType (cepRawValue) {
+  const cepTypeOf = typeof cepRawValue
 
-      if (cepTypeOf === 'number' || cepTypeOf === 'string') {
-        return cepRawValue
-      }
+  if (cepTypeOf === 'number' || cepTypeOf === 'string') {
+    return cepRawValue
+  }
 
-      throw new CepPromiseError({
-        message: 'Erro ao inicializar a instância do CepPromise.',
-        type: 'validation_error',
-        errors: [{
-          message: 'Você deve chamar o construtor utilizando uma String ou um Number.',
-          service: 'cep_validation'
-        }]
-      })
-    }
+  throw new CepPromiseError({
+    message: 'Erro ao inicializar a instância do CepPromise.',
+    type: 'validation_error',
+    errors: [{
+      message: 'Você deve chamar o construtor utilizando uma String ou um Number.',
+      service: 'cep_validation'
+    }]
+  })
+}
 
-    function removeSpecialCharacters (cepRawValue) {
-      return cepRawValue.toString().replace(/\D+/g, '')
-    }
+function removeSpecialCharacters (cepRawValue) {
+  return cepRawValue.toString().replace(/\D+/g, '')
+}
 
-    function leftPadWithZeros (cepCleanValue) {
-      return '0'.repeat(CEP_SIZE - cepCleanValue.length) + cepCleanValue
-    }
+function leftPadWithZeros (cepCleanValue) {
+  return '0'.repeat(CEP_SIZE - cepCleanValue.length) + cepCleanValue
+}
 
-    function validateInputLength (cepWithLeftPad) {
-      if (cepWithLeftPad.length <= CEP_SIZE) {
-        return cepWithLeftPad
-      }
+function validateInputLength (cepWithLeftPad) {
+  if (cepWithLeftPad.length <= CEP_SIZE) {
+    return cepWithLeftPad
+  }
 
-      throw new CepPromiseError({
-        message: `CEP deve conter exatamente ${CEP_SIZE} caracteres.`,
-        type: 'validation_error',
-        errors: [{
-          message: `CEP informado possui mais do que ${CEP_SIZE} caracteres.`,
-          service: 'cep_validation'
-        }]
-      })
-    }
+  throw new CepPromiseError({
+    message: `CEP deve conter exatamente ${CEP_SIZE} caracteres.`,
+    type: 'validation_error',
+    errors: [{
+      message: `CEP informado possui mais do que ${CEP_SIZE} caracteres.`,
+      service: 'cep_validation'
+    }]
+  })
+}
 
-    function fetchCepFromServices (cepWithLeftPad) {
-      return Promise.any([
-        fetchCorreios(cepWithLeftPad),
-        fetchViaCep(cepWithLeftPad)
-      ])
-    }
+function fetchCepFromServices (cepWithLeftPad) {
+  return Promise.any([
+    fetchCorreios(cepWithLeftPad),
+    fetchViaCep(cepWithLeftPad)
+  ])
+}
 
-    function resolvePromise (addressObject) {
-      resolve(addressObject)
-    }
+function handleServicesError (aggregatedErrors) {
+  if (aggregatedErrors.length !== undefined) {
+    throw new CepPromiseError({
+      message: 'Todos os serviços de CEP retornaram erro.',
+      type: 'service_error',
+      errors: aggregatedErrors
+    })
+  }
+  throw aggregatedErrors
+}
 
-    function handleServicesError (aggregatedErrors) {
-      if (aggregatedErrors.length !== undefined) {
-        throw new CepPromiseError({
-          message: 'Todos os serviços de CEP retornaram erro.',
-          type: 'service_error',
-          errors: aggregatedErrors
-        })
-      }
-      throw aggregatedErrors
-    }
-
-    function rejectWithCepPromiseError (error) {
-      reject(new CepPromiseError({
-        message: error.message,
-        type: error.type,
-        errors: error.errors
-      }))
-    }
-
+function throwApplicationError (error) {
+  throw new CepPromiseError({
+    message: error.message,
+    type: error.type,
+    errors: error.errors
   })
 }
--- src/services/viacep.js
+++ src/services/viacep.js
@@ -4,69 +4,59 @@ import fetch from 'isomorphic-fetch'
 import ServiceError from '../errors/service.js'
 
 function fetchViaCepService (cepWithLeftPad) {
-
-  return new Promise((resolve, reject) => {
-    const url = `https://viacep.com.br/ws/${cepWithLeftPad}/json/`
-    const options = {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'content-type': 'application/json;charset=utf-8'
-      }
+  const url = `https://viacep.com.br/ws/${cepWithLeftPad}/json/`
+  const options = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'content-type': 'application/json;charset=utf-8'
     }
+  }
 
-    return fetch(url, options)
-      .then(analyzeAndParseResponse)
-      .then(checkForViaCepError)
-      .then(extractCepValuesFromResponse)
-      .then(resolvePromise)
-      .catch(rejectWithServiceError)
+  return fetch(url, options)
+    .then(analyzeAndParseResponse)
+    .then(checkForViaCepError)
+    .then(extractCepValuesFromResponse)
+    .catch(throwApplicationError)
+}
 
-    function analyzeAndParseResponse (response) {
-      if (response.ok) {
-        return response.json()
-      }
+ function analyzeAndParseResponse (response) {
+  if (response.ok) {
+    return response.json()
+  }
 
-      throw Error('Erro ao se conectar com o serviço ViaCEP.')
-    }
+  throw Error('Erro ao se conectar com o serviço ViaCEP.')
+}
 
-    function checkForViaCepError (responseObject) {
-      if (responseObject.erro === true) {
-        throw new Error('CEP não encontrado na base do ViaCEP.')
-      }
+function checkForViaCepError (responseObject) {
+  if (responseObject.erro === true) {
+    throw new Error('CEP não encontrado na base do ViaCEP.')
+  }
 
-      return responseObject
-    }
+  return responseObject
+}
 
-    function extractCepValuesFromResponse (responseObject) {
-      return {
-        cep: responseObject.cep.replace('-', ''),
-        state: responseObject.uf,
-        city: responseObject.localidade,
-        neighborhood: responseObject.bairro,
-        street: responseObject.logradouro
-      }
-    }
+function extractCepValuesFromResponse (responseObject) {
+  return {
+    cep: responseObject.cep.replace('-', ''),
+    state: responseObject.uf,
+    city: responseObject.localidade,
+    neighborhood: responseObject.bairro,
+    street: responseObject.logradouro
+  }
+}
 
-    function resolvePromise (cepObject) {
-      resolve(cepObject)
-    }
-
-    function rejectWithServiceError (error) {
-      const serviceError = new ServiceError({
-        message: error.message,
-        service: 'viacep'
-      })
-
-      if (error.name === 'FetchError') {
-        serviceError.message = 'Erro ao se conectar com o serviço ViaCEP.'
-      }
-
-      reject(serviceError)
-    }
-
+function throwApplicationError (error) {
+  const serviceError = new ServiceError({
+    message: error.message,
+    service: 'viacep'
   })
 
+  if (error.name === 'FetchError') {
+    serviceError.message = 'Erro ao se conectar com o serviço ViaCEP.'
+  }
+
+  throw serviceError;
 }
 
 export default fetchViaCepService
