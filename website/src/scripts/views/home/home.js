9607f15dfc5c1c5d9717e6a26c70489aafd12575
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-10 20:51:30 -0200

    Include slice to home

--- website/src/scripts/views/home/home.js
+++ website/src/scripts/views/home/home.js
@@ -1,10 +1,12 @@
+import slice from '@scripts/components/slice/slice';
 import viewport from '@scripts/components/viewport/viewport';
 import template from './home.html';
 
 export default {
   name: 'home',
   components: {
-    viewport
+    viewport,
+    slice
   },
   template
 };
