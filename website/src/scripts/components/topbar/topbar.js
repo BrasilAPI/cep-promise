a88b0a6844518c160b5659109b65ff7c8537d97f
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-10 20:50:33 -0200

    Include navmenu in topbar

--- website/src/scripts/components/topbar/topbar.js
+++ website/src/scripts/components/topbar/topbar.js
@@ -1,13 +1,15 @@
 import '@styles/topbar.styl';
 import container from '@scripts/components/container/container';
 import logo from '@scripts/components/logo/logo';
+import navmenu from '@scripts/components/navmenu/navmenu';
 import template from './topbar.html';
 
 export default {
   name: 'topbar',
   components: {
     container,
-    logo
+    logo,
+    navmenu
   },
   template
 };
--- website/src/scripts/components/topbar/topbar.test.js
+++ website/src/scripts/components/topbar/topbar.test.js
@@ -1,12 +1,18 @@
 import { shallowMount } from '@vue/test-utils';
 import container from '@scripts/components/container/container';
 import logo from '@scripts/components/logo/logo';
+import navmenu from '@scripts/components/navmenu/navmenu';
 import topbar from './topbar';
 
 describe('Topbar', () => {
   function mountComponent(){
     return shallowMount(topbar);
   }
+
+  it('should have appropriate css classes', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.classes()).toEqual(['topbar']);
+  });
 
   it('should contain a container', () => {
     const wrapper = mountComponent();
@@ -16,5 +22,10 @@ describe('Topbar', () => {
   it('should contain a logo', () => {
     const wrapper = mountComponent();
     expect(wrapper.find(logo)).toBeDefined();
+  });
+
+  it('should contain a navigation menu', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.find(navmenu)).toBeDefined();
   });
 });
