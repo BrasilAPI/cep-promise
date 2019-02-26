a0d4530d2164acfc7086cd99bf901ff2d6aa0634
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-25 22:59:54 -0300

    Create intro component

---
+++ website/src/scripts/components/intro/intro.js
@@ -0,0 +1,15 @@
+import '@styles/intro.styl';
+import btn from '@scripts/components/btn/btn';
+import row from '@scripts/components/row/row';
+import rowItem from '@scripts/components/row-item/row-item';
+import template from './intro.html';
+
+export default {
+  name: 'intro',
+  components: {
+    btn,
+    row,
+    rowItem
+  },
+  template
+};
