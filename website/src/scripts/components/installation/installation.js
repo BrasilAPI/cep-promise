3d0507d5e6e7bacb9cd5343ed6177dcaf54b9e97
Rafael Camargo <hello@rafaelcamargo.com>
2019-03-03 22:06:55 -0300

    Create installation

---
+++ website/src/scripts/components/installation/installation.js
@@ -0,0 +1,11 @@
+import '@styles/installation.styl';
+import codeSample from '@scripts/components/code-sample/code-sample';
+import template from './installation.html';
+
+export default {
+  name: 'installation',
+  components: {
+    codeSample
+  },
+  template
+};
