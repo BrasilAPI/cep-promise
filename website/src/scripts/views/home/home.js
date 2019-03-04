de03b9d7f0daeaab2fc0f3a8f21746bbeb6e38aa
Rafael Camargo <hello@rafaelcamargo.com>
2019-03-03 22:07:08 -0300

    Add installation to home view

--- website/src/scripts/views/home/home.js
+++ website/src/scripts/views/home/home.js
@@ -1,4 +1,5 @@
 import featureList from '@scripts/components/feature-list/feature-list';
+import installation from '@scripts/components/installation/installation';
 import intro from '@scripts/components/intro/intro';
 import slice from '@scripts/components/slice/slice';
 import viewport from '@scripts/components/viewport/viewport';
@@ -8,6 +9,7 @@ export default {
   name: 'home',
   components: {
     featureList,
+    installation,
     intro,
     slice,
     viewport
