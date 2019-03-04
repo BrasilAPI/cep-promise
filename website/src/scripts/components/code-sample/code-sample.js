b6881aea96f8ab67680ecee4f70d5c072e3e150b
Rafael Camargo <hello@rafaelcamargo.com>
2019-03-03 22:06:15 -0300

    Create code sample

---
+++ website/src/scripts/components/code-sample/code-sample.js
@@ -0,0 +1,29 @@
+import '@styles/code-sample.styl';
+import 'prismjs/themes/prism.css';
+import Prism from 'prismjs';
+import template from './code-sample.html';
+
+export default {
+  name: 'code-sample',
+  props: ['language'],
+  mounted(){
+    if(this.language)
+      this.renderCodeSample(this.highlightCode(this.getCodeSample(), this.language));
+  },
+  methods: {
+    getCodeSample(){
+      return this.getCodeSampleContainer().innerText;
+    },
+    highlightCode(code, language){
+      return Prism.highlight(code, Prism.languages[language], language);
+    },
+    renderCodeSample(highlightedCode){
+      const container = this.getCodeSampleContainer();
+      container.innerHTML = highlightedCode;
+    },
+    getCodeSampleContainer(){
+      return this.$el.querySelector('[data-code-sample]');
+    }
+  },
+  template
+};
---
+++ website/src/scripts/components/code-sample/code-sample.test.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import { mount } from '@vue/test-utils';
+import Prism from 'prismjs';
+import codeSample from './code-sample';
+
+describe('Code Sample', () => {
+  function mountComponent(propsData, slots){
+    return mount(codeSample, { propsData, slots });
+  }
+
+  beforeEach(() => {
+    Prism.highlight = jest.fn(code => code);
+  });
+
+  it('should have appropriate css classes', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.classes()).toEqual(['code-sample']);
+  });
+
+  it('should render a plain text if no language has been provided', () => {
+    const plainText = 'npm install cep-promise --save';
+    const wrapper = mountComponent({}, { default: plainText });
+    expect(wrapper.find('code').text()).toEqual(plainText);
+  });
+
+  it('should render a highlighted code if language has been provided', () => {
+    const code = '<p>Hello</p>';
+    const Constructor = Vue.extend(codeSample);
+    const vm = new Constructor({
+      propsData: {language: 'html'},
+      slots: { default: code }
+    }).$mount();
+    vm.$el.querySelector = jest.fn(() => {
+      return { innerText: code };
+    });
+    vm.$mount();
+    expect(Prism.highlight.mock.calls[1][0]).toEqual(code);
+    expect(typeof Prism.highlight.mock.calls[1][1]).toEqual('object');
+    expect(Prism.highlight.mock.calls[1][2]).toEqual('html');
+    expect(vm.$el.querySelector('code').innerText).toEqual(code);
+  });
+});
