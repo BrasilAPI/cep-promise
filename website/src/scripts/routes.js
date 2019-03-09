4929ad80d7b9a7315391f264f7217311bb525995
Rafael Camargo <hello@rafaelcamargo.com>
2019-03-09 18:03:44 -0300

    Create trial view

--- website/src/scripts/routes.js
+++ website/src/scripts/routes.js
@@ -1,11 +1,17 @@
 import home from '@scripts/views/home/home';
 import use from '@scripts/views/use/use';
+import trial from '@scripts/views/trial/trial';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: home
+  },
+  {
+    path: '/experimente',
+    name: 'trial',
+    component: trial
   },
   {
     path: '/use',
---
+++ website/src/scripts/views/trial/trial.js
@@ -0,0 +1,14 @@
+import sandbox from '@scripts/components/sandbox/sandbox';
+import slice from '@scripts/components/slice/slice';
+import viewport from '@scripts/components/viewport/viewport';
+import template from './trial.html';
+
+export default {
+  name: 'trial-view',
+  components: {
+    sandbox,
+    slice,
+    viewport
+  },
+  template
+};
