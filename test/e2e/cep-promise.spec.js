0c760aebd2a9f164c4a33baa17efb7b9c7628f59
Luciano Franca <lucianopf@outlook.com>
2019-04-29 18:54:29 -0300

    tests: adapt to stop using cepaberto service

--- test/e2e/cep-promise.spec.js
+++ test/e2e/cep-promise.spec.js
@@ -48,7 +48,7 @@ describe('cep-promise (E2E)', () => {
               message: 'Todos os serviços de CEP retornaram erro.',
               type: 'service_error',
               errors: [{
-                message: 'CEP NAO ENCONTRADO',
+                message: 'CEP INVÁLIDO',
                 service: 'correios'
               }, {
                 message: 'CEP não encontrado na base do ViaCEP.',
